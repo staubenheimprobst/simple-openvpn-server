8501e77a02ec41100ec910003227777777596cd9
STP <s.taubenheim@tux-mania.de>
2019-03-23 14:34:41 +0100

    changes in update.sh

--- update.sh
+++ update.sh
@@ -40,34 +40,34 @@ fi
 
 #udate the webserver scripts
 if [[ "$OS"='openwrt' ]]; then
-        mkdir /www2
-        mkdir /www2/css
-        mkdir /www2/images
+        #mkdir /www2
+        #mkdir /www2/css
+        #mkdir /www2/images
         wget -O /www2/index.sh $HTTPGIT/index.sh
-        wget -O /www2/download.sh $HTTPGIT/download.sh
-        wget -O /www2/config.sh $HTTPGIT/config.sh
-        wget -O /www2/VARS.sh $HTTPGIT/VARS.sh
-        wget -O /www2/LOAD.sh $HTTPGIT/LOAD.sh
-        wget -O /www2/admin.sh $HTTPGIT/admin.sh
+        wget -O /www2/admin/download.sh $HTTPGIT/download.sh
+        wget -O /www2/admin/config.sh $HTTPGIT/config.sh
+        wget -O /www2/admin/VARS.sh $HTTPGIT/VARS.sh
+        wget -O /www2/admin/LOAD.sh $HTTPGIT/LOAD.sh
+        wget -O /www2/admin/admin.sh $HTTPGIT/admin.sh
         wget -O /www2/head_tmp $HTTPGIT/head_tmp
         wget -O /www2/css/bootstrap.min.css $HTTPGIT/css/bootstrap.min.css
         wget -O /www2/images/favicon.png $HTTPGIT/images/favicon.png
-        chown -R http:nogroup /www2
-        chown -R http:nogroup /etc/openvpn/ccd
+        #chown -R http:nogroup /www2
+        #chown -R http:nogroup /etc/openvpn/ccd
 else
-        rm /var/www/html/*
-        mkdir /var/www/html/css
+        #rm /var/www/html/*
+        #mkdir /var/www/html/css
         wget -O /var/www/html/index.sh $HTTPGIT/index.sh
-        wget -O /var/www/html/admin.sh $HTTPGIT/admin.sh
-        wget -O /var/www/html/download.sh $HTTPGIT/download.sh
-        wget -O /var/www/html/config.sh $HTTPGIT/config.sh
-        wget -O /var/www/html/VARS.sh $HTTPGIT/VARS.sh
-        wget -O /var/www/html/LOAD.sh $HTTPGIT/LOAD.sh
+        wget -O /var/www/html/admin/admin.sh $HTTPGIT/admin.sh
+        wget -O /var/www/html/admin/download.sh $HTTPGIT/download.sh
+        wget -O /var/www/html/admin/config.sh $HTTPGIT/config.sh
+        wget -O /var/www/html/admin/VARS.sh $HTTPGIT/VARS.sh
+        wget -O /var/www/html/admin/LOAD.sh $HTTPGIT/LOAD.sh
         wget -O /var/www/html/head_tmp  $HTTPGIT/head_tmp
         wget -O /var/www/html/css/bootstrap.min.css $HTTPGIT/css/bootstrap.min.css
         wget -O /var/www/html/images/favicon.png $HTTPGIT/images/favicon.png
-        chown -R www-data:www-data /var/www/html/
-        chown -R www-data:www-data /etc/openvpn/ccd
+        #chown -R www-data:www-data /var/www/html/
+        #chown -R www-data:www-data /etc/openvpn/ccd
 fi
 
 exit 0
